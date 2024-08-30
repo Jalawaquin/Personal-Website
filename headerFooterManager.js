f278b56a65b011e3b4d6d95b539a943cce6ad0a8
Jalawaquin <57761181+Jalawaquin@users.noreply.github.com>
2024-08-30 18:40:44 -0500

    test design for mobile
    in process Redesigning website for mobile

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -10,26 +10,4 @@ class SpecialHeader extends HTMLElement{
     }
 }
 
-class SpecialNav extends HTMLElement{
-    connectedCallback(){
-        this.innerHTML = `
-        <nav>
-          <a href="about.html">
-            <img src="NAV_IMAGES/ABOUT.png" alt="cloud music" class="nav-img">
-          </a>
-          <a href="music.html">
-            <img src="NAV_IMAGES/MUSIC.png" alt="cloud music" class="nav-img">
-          </a>
-            <a href="videos.html">
-              <img src="NAV_IMAGES/VIDEOS.png" alt="cloud videos" class="nav-img">
-            </a>
-          <a href="contact.html">
-            <img src="NAV_IMAGES/CONTACT.png" alt="cloud contact" class="nav-img">
-          </a>
-        </nav>
-        `
-    }    
-}
-
 customElements.define('special-header', SpecialHeader)
-customElements.define('special-nav',SpecialNav)
